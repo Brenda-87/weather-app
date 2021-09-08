8546580b32e01b57101f279d7e40df34cdc6da99
Brenda <brenda@Rolands-MacBook-Pro.local>
2021-09-08 15:54:47 +0200

    Updated array in forecast
    follow up

--- src/script.js
+++ src/script.js
@@ -38,9 +38,9 @@ function handleForecastResponse(response) {
   let forecastDay5 = document.querySelector("#forecast-5");
   forecastDay1.innerHTML = Math.round(response.data.list[8].main.temp) + "°";
   forecastDay2.innerHTML = Math.round(response.data.list[16].main.temp) + "°";
-  forecastDay3.innerHTML = Math.round(response.data.list[24].main.temp) + "°";
-  forecastDay4.innerHTML = Math.round(response.data.list[32].main.temp) + "°";
-  forecastDay5.innerHTML = Math.round(response.data.list[40].main.temp) + "°";
+  forecastDay3.innerHTML = Math.round(response.data.list[15].main.temp) + "°";
+  forecastDay4.innerHTML = Math.round(response.data.list[22].main.temp) + "°";
+  forecastDay5.innerHTML = Math.round(response.data.list[29].main.temp) + "°";
 }
 
 function handleWeatherResponse(response) {
